edcbf9fff2b4eaa56f3b62c2e6207ffd1f639826
NazaryIndy <galya.pryadkina@gmail.com>
2025-11-04 17:13:09 +0300

    Add structure

---
+++ src/pages/JobDetailsPage/JobDetailsPage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+type JobDetailsPageProps = {};
+
+const JobDetailsPage: FC<JobDetailsPageProps> = () => {
+    return <div />;
+};
+
+export { JobDetailsPage, type JobDetailsPageProps };
---
+++ src/pages/JobsListPage/JobsListPage.tsx
@@ -0,0 +1,9 @@
+import type { FC } from 'react';
+
+type JobsListPageProps = {};
+
+const JobsListPage: FC<JobsListPageProps> = () => {
+    return <div />;
+};
+
+export { JobsListPage, type JobsListPageProps };
